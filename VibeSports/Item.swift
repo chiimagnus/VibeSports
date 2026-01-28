16d062ca5b6f0f50cac9f26fb5ef84aef5cdbc47
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-01-28 18:56:26 +0800

    Initial Commit

---
+++ VibeSports/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  VibeSports
+//
+//  Created by chii_magnus on 2026/1/28.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
